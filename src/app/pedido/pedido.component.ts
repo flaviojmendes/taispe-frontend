3345ef87104e41b3f167a942faf73fff52857d47
Mendes <Flavio.Mendes@broadridge.com>
2020-05-18 14:33:04 +0100

    ajustando layout

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -9,16 +9,25 @@ export class PedidoComponent implements OnInit {
 
   constructor() { }
 
-  qtdDisponivel: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  item1: number;
-  item2: number;
-  item3: number;
+  qtdDisponivel: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  refrisDisponiveis: number[] = [0];
+  item1 = 0;
+  item2 = 0;
+  item3 = 0;
   pedido: string;
   observacoes = '';
   total: number;
 
+  cocaCola = 0;
+  guarana = 0;
+  totalRefris = 0;
+  totalRefrisDisponivel = 0;
+
+  valido = false;
+
   ngOnInit() {
     this.atualizarValor();
+    this.valida();
   }
 
   atualizarValor() {
@@ -26,6 +35,18 @@ export class PedidoComponent implements OnInit {
     this.total += this.item1 ? (this.item1 * 9.99) : 0;
     this.total += this.item2 ? (this.item2 * 11.99) : 0;
     this.total += this.item3 ? (this.item3 * 4) : 0;
+    this.atualizarRefrisDisponiveis();
+    this.valida();
+  }
+
+  atualizarRefrisDisponiveis() {
+    this.totalRefris = this.cocaCola + this.guarana;
+    this.totalRefrisDisponivel = this.item1;
+    this.refrisDisponiveis = [];
+    for (let i = 0 ; i <= this.item1 ; i++) {
+      this.refrisDisponiveis.push(i);
+    }
+    this.valida();
   }
 
   atualizarPedido() {
@@ -41,6 +62,19 @@ export class PedidoComponent implements OnInit {
       observacoes;
 
     this.pedido = encodeURIComponent(this.pedido);
+  }
+
+  valida() {
+    if (this.item1 === 0 && this.item2 === 0 && this.item3 === 0) {
+      this.valido = false;
+      return;
+    }
+
+    if(this.cocaCola + this.guarana > this.totalRefrisDisponivel) {
+      this.valido = false;
+      return;
+    }
+    this.valido = true;
   }
 
 }
