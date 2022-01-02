ab57f9c79dae6b4b6782433435b6e612d54c567d
Mendes <flaviojmendes@gmail.com>
2022-01-02 21:07:35 +0000

    upgrading angular

--- src/app/company/company.component.spec.ts
+++ src/app/company/company.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CompanyComponent } from './company.component';
 
@@ -6,7 +6,7 @@ describe('CompanyComponent', () => {
   let component: CompanyComponent;
   let fixture: ComponentFixture<CompanyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CompanyComponent ]
     })
