211ee5c4819608352dc82b28f1263710691bb06b
Mendes <flaviojmendes@gmail.com>
2022-01-04 17:31:36 +0000

    adding currency

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backendUrl: 'https://menu.taispe.com:8181',
   authUrl: 'https://menu.taispe.com/auth',
-  cliendID: '6f14aMt50o8SLp3IndysC56UXarlT5Wl'
+  clientID: '6f14aMt50o8SLp3IndysC56UXarlT5Wl'
 };
